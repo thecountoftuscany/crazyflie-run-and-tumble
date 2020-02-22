5824e2dafc4995ffe19716236a69b9ef678e3d08
nishant@me586vm <14308383+thecountoftuscany@users.noreply.github.com>
2020-02-22 12:56:26 -0800

    Added crazyflie-firmware with working deck driver for BH1750 light sensor

---
+++ crazyflie-firmware/src/deck/drivers/src/bh1750deck.c
@@ -0,0 +1,92 @@
+#define DEBUG_MODULE "bh1750Deck"
+// All includes
+#include "debug.h"
+#include "i2cdev.h"
+#include "deck.h"
+#include "log.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "system.h"
+
+// Addresses, constants etc for BH1750
+#define BH1750I2CAddr 0x23
+#define ONE_TIME_HI_RES_MODE 0x20
+#define ONE_TIME_HI_RES_MODE2 0x21
+#define ONE_TIME_LOW_RES_MODE 0x23
+#define CONT_HI_RES_MODE 0x10
+#define CONT_HI_RES_MODE2 0x11
+#define CONT_LOW_RES_MODE 0x13
+float conv_factor = 1.2;
+#define BH1750_TASK_STACKSIZE    (2 * configMINIMAL_STACK_SIZE)
+#define BH1750_TASK_PRI 3
+#define BH1750_TASK_NAME "BH1750"
+
+// Misc global variables
+static bool isInit;
+float intensity;
+void bh1750Task(void* arg);
+
+// Deck driver init function
+static void bh1750Init()
+{
+  if (isInit)
+    return;
+
+  DEBUG_PRINT("Initializing BH1750...\n");
+  i2cdevInit(I2C1_DEV);
+
+  xTaskCreate(bh1750Task, BH1750_TASK_NAME, BH1750_TASK_STACKSIZE, NULL, BH1750_TASK_PRI, NULL);
+
+  isInit = true;
+  DEBUG_PRINT("Initialization complete!\n");
+}
+
+// Deck driver test function
+static bool bh1750Test()
+{
+  DEBUG_PRINT("BH1750Deck test\n");
+  return true;
+}
+
+void bh1750Task(void* arg)
+{
+  uint8_t dataHB;
+  uint8_t dataLB;
+  bool ack;
+  uint8_t intensitydata[2];
+
+  systemWaitStart();
+  TickType_t xLastWakeTime;
+
+  // Write mode to sensor
+  i2cdevWriteByte(I2C1_DEV, (uint8_t)BH1750I2CAddr, I2CDEV_NO_MEM_ADDR, (uint8_t)CONT_HI_RES_MODE);
+  //i2cdevWrite(I2C1_DEV, (uint8_t)BH1750I2CAddr, 1, (uint8_t)CONT_HI_RES_MODE);
+
+  xLastWakeTime = xTaskGetTickCount();
+
+  while (1) {
+    vTaskDelayUntil(&xLastWakeTime, M2T(120));
+   
+    // Read two bytes from the sensor
+    //i2cdevReadByte(I2C1_DEV, (uint8_t)BH1750I2CAddr, I2CDEV_NO_MEM_ADDR, &dataHB);
+    //i2cdevReadByte(I2C1_DEV, (uint8_t)BH1750I2CAddr, I2CDEV_NO_MEM_ADDR, &dataLB);
+    i2cdevRead(I2C1_DEV, (uint8_t)BH1750I2CAddr, 2, &intensitydata);
+    // Combine the two bytes to get data
+    //intensity = dataHB << 8 | dataLB;
+    intensity = intensitydata[0] << 8 | intensitydata[1];
+    intensity = intensity/conv_factor;
+    //DEBUG_PRINT("Light reading is: %f\n", data);
+  }
+}
+
+
+static const DeckDriver bh1750Driver = {
+  .name = "bh1750Deck",
+  .init = bh1750Init,
+  .test = bh1750Test,
+};
+
+DECK_DRIVER(bh1750Driver);
+LOG_GROUP_START(BH1750)
+LOG_ADD(LOG_FLOAT, intensity, &intensity)
+LOG_GROUP_STOP(BH1750)
---
+++ crazyflie-firmware/tools/make/config.mk
@@ -0,0 +1,98 @@
+# Hello deck
+#CFLAGS += -DDECK_FORCE=myHello
+#DEBUG=1
+
+# bh1750 deck
+CFLAGS += -DDECK_FORCE=bh1750Deck
+DEBUG=1
+
+# tsl2591 deck
+#CFLAGS += -DDECK_FORCE=tsl2591Deck
+#DEBUG=1
+
+## Copy this file to config.mk and modify to get you personal build configuration
+
+## Force device type string
+# CFLAGS += -DDEVICE_TYPE_STRING_FORCE="CF20"
+
+## Force a sensor implementation to be used
+# SENSORS=bosch
+
+## Set CRTP link to E-SKY receiver
+# CFLAGS += -DUSE_ESKYLINK
+
+## Redirect the console output to the UART
+# CFLAGS += -DDEBUG_PRINT_ON_UART
+
+## Redirect the console output to JLINK (using SEGGER RTT)
+# DEBUG_PRINT_ON_SEGGER_RTT = 1
+
+## Load a deck driver that has no OW memory
+# CFLAGS += -DDECK_FORCE=bcBuzzer
+
+## Load multiple deck drivers that has no OW memory
+# CFLAGS += -DDECK_FORCE=bcBuzzer:bcLedRing
+
+## Enable biq quad deck features
+# CFLAGS += -DENABLE_BQ_DECK
+# CFLAGS += -DBQ_DECK_ENABLE_PM
+# CFLAGS += -DBQ_DECK_ENABLE_OSD
+
+## Use morse when flashing the LED to indicate that the Crazyflie is calibrated
+# CFLAGS += -DCALIBRATED_LED_MORSE
+
+## Disable LEDs from turning on/flashing
+# CFLAGS += -DTURN_OFF_LEDS
+
+## Set the default LED Ring effect (if not set, effect 6 will be used)
+# CFLAGS += -DLEDRING_DEFAULT_EFFECT=0
+
+## Set LED Rings to use less LEDs
+# CFLAGS += -DLED_RING_NBR_LEDS=6
+
+## Set LED Rings to use less more LEDs (only if board is modified)
+# CFLAGS += -DLED_RING_NBR_LEDS=24
+
+## Turn on monitoring of queue usages
+# CFLAGS += -DDEBUG_QUEUE_MONITOR
+
+## Automatically reboot to bootloader before flashing
+# CLOAD_CMDS = -w radio://0/100/2M/E7E7E7E7E7
+
+## Set number of anchor in LocoPositioningSystem
+# CFLAGS += -DLOCODECK_NR_OF_ANCHORS=8
+
+## Set alternative pins for LOCO deck (IRQ=IO_2, RESET=IO_3, default are RX1 and TX1)
+# CFLAGS += -DLOCODECK_USE_ALT_PINS
+
+## Disable Low Interference Mode when using Loco Deck
+# CFLAGS += -DLOCODECK_NO_LOW_INTERFERENCE
+
+## Set the positioning system in TDoA2 mode on startup
+# LPS_TDOA_ENABLE=1
+
+## Low interference communication
+# Set the 'low interference' 2.4GHz TX power. This power is set when the loco deck is initialized
+# Possible power are: +4, 0, -4, -8, -12, -16, -20, -30
+# CFLAGS += -DPLATFORM_NRF51_LOW_INTERFERENCE_TX_POWER_DBM="(-12)"
+
+
+## Use J-Link as Debugger/flasher
+# OPENOCD_INTERFACE ?= interface/jlink.cfg
+# OPENOCD_TARGET    ?= target/stm32f4x.cfg
+# OPENOCD_CMDS      ?= -c "transport select swd"
+
+
+## TDaA 3 - experimental -------------------------------------------
+# Enable 2D positioning. The value (1.2) is the height that the tag will move at
+# Only use in TDoA 3
+# CFLAGS += -DLPS_2D_POSITION_HEIGHT=1.2
+
+# Enable longer range (lower bit rate)
+# Only use in TDoA 3
+# Note: Anchors must also be built with this flag
+# CFLAGS += -DLPS_LONGER_RANGE
+
+## SDCard test configuration ------------------------------------
+# FATFS_DISKIO_TESTS  = 1	# Set to 1 to enable FatFS diskio function tests. Erases card.
+
